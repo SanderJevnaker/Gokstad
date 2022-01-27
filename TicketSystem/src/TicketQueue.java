7857a264d3dc11a1d1eda38495281db083d33e35
SanderJevnaker <sanderjevnaker@hotmail.com>
2022-01-27 09:08:53 +0100

    First commit

--- TicketSystem/src/TicketQueue.java
+++ TicketSystem/src/TicketQueue.java
@@ -6,26 +6,30 @@ public class TicketQueue {
 
     }
 
+    public void showMenu() {
+        System.out.println("1 - Add Ticket");
+        System.out.println("2 - Serve Customer");
+        System.out.println("3 - Print Queue");
+        System.out.println("4 - Quit");
+
+    }
+
+    public void ticketMenu(String choice) {
+        switch (choice) {
+            case "1" -> ticket();
+            case "2" -> serveCustomer();
+            case "3" -> printQueue();
+            case "4" -> quit();
+            default -> System.out.println("Invalid menu choice, please choose 1-4.");
+        }
+    }
     public void ticketMenu() {
         Scanner scan = new Scanner(System.in);
 
-
         while (true) {
-            System.out.println("1 - Add Ticket");
-            System.out.println("2 - Serve Customer");
-            System.out.println("3 - Print Queue");
-            System.out.println("4 - Quit");
-
+            showMenu();
             String choice = scan.nextLine();
-
-
-            switch (choice) {
-                case "1" -> ticket();
-                case "2" -> serveCustomer();
-                case "3" -> printQueue();
-                case "4" -> quit();
-                default -> System.out.println("Invalid menu choice, please choose 1-4.");
-            }
+            ticketMenu(choice);
         }
     }
     public void ticket(){
@@ -43,4 +47,13 @@ public class TicketQueue {
         System.out.println("System is shutting down...");
         System.exit(0);
     }
+    public static void main (String[] args) {
+        print();
+    }
+
+    static void print() {
+        TicketQueue ticketQueue = new TicketQueue();
+        ticketQueue.ticketMenu();
+    }
 }
+
